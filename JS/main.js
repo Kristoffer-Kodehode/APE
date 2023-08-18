e105cb389b00965c3f3c7397285d5a6c45fea64a
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-08-18 08:35:37 +0200

    added a few things, made a few changes

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,71 @@
 "use strict";
 
 const contentContainer = document.getElementById("container");
-const navBtn = document.getElementById("nav-btn");
+const homeBtn = document.getElementById("home-btn");
+const renderBtn = document.getElementById("render-btn");
+const dogBtn = document.getElementById("dog-btn");
+const catBtn = document.getElementById("cat-btn");
 
-const baseURL = "https://shibe.online/api/shibes?";
+let baseFactURL = "";
+let baseImgURL = "https://shibe.online/api/";
+let imgURL = "";
+
 // other attempted APIs: "https://random-d.uk/api"https://randomfox.ca/floof/"https://randomfox.ca/images/"
-let navState = "main";
+let navState = "home";
 
-navBtn.addEventListener("click", () => {
-  navState = "main";
-  navigate(`${baseURL}count=${magicNumber(100, 1)}&urls=true&httpsUrls=true.jpg`);
+renderBtn.addEventListener("click", () => {
+  if (navState === "dog") {
+    imgURL = `${baseImgURL}shibes`;
+    baseFactURL = "http://dog-api.kinduff.com/api/facts";
+    renderContent(`${imgURL}?count=1&urls=true&httpsUrls=true.jpg`, `${baseFactURL}?number=50}`);
+  } else if (navState === "cat") {
+    imgURL = `${baseImgURL}cats`;
+    baseFactURL = "https://meowfacts.herokuapp.com/";
+    renderContent(`${imgURL}?count=1&urls=true&httpsUrls=true.jpg`, `${baseFactURL}?count=50}`);
+  }
 });
 
-function renderDog(dog) {
+homeBtn.addEventListener("click", () => location.reload());
+
+dogBtn.addEventListener("click", () => {
+  navState = "dog";
+  console.log(navState);
+});
+
+catBtn.addEventListener("click", () => {
+  navState = "cat";
+  console.log(navState);
+});
+
+function renderAnimal(animal) {
   const render = document.createElement("img");
   contentContainer.append(render);
-  render.src = dog;
+  render.src = animal;
 }
 
-async function navigate(url) {
-  const data = await fetchData(url);
+function renderFact(fact) {
+  const factoid = document.createElement("p");
+  factoid.className = "factoid";
+  factoid.textContent = fact;
+  contentContainer.append(factoid);
+}
 
+async function renderContent(imgURL, factURL) {
+  const imgData = await fetchData(imgURL);
+  const factData = await fetchData(factURL);
+  //factData.keys[0]
   contentContainer.innerHTML = "";
 
-  if (navState === "main") {
-    const doggy = data[magicNumber(data.length, 1)];
-    console.log(doggy);
-    renderDog(doggy);
+  const animal = imgData[0];
+  const fact = navState === "dog" ? factData.facts[0] : factData.data[0];
+  if (animal !== "undefined") {
+    renderAnimal(animal);
+    console.log(animal, fact);
+    renderFact(fact);
   } else {
     console.log(data);
-    renderDogDetails(data);
   }
+  if ((renderBtn.textContent = "RENDER")) renderBtn.textContent = "MORE!";
 }
 
 async function fetchData(url) {
